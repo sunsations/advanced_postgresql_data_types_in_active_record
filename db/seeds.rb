e521335a588540e36b4af5c05d454aaaf63397e8
Thomas Arni <thomas.arni@gmail.com>
2015-04-09 10:11:03 +0200

    populate some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+
+(1..1000).each do |i|
+  product = if i % 3 == 0
+    Product.create(:name => Faker::Commerce.product_name, :data => {'author' => Faker::Name.name, 'pages' => rand(100..600), 'category' => Faker::Lorem.word})
+  else
+    Product.create(:name => Faker::Commerce.product_name, :data => {'author' => Faker::Name.name, 'pages' => rand(100..600), 'category' => Faker::Lorem.word, 'isbn' => Faker::Code.isbn})
+  end
+  puts product.inspect
+end
+
+puts Product.last.data['category']  # => 'fiction'
